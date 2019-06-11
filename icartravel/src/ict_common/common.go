2d79c9edeb4347e1968ade646475dc5ff35bdcda
75912001 <75912001@qq.com>
2019-06-11 11:37:23 +0800

    111

--- icartravel/src/ict_common/common.go
+++
@@ -1,7 +0,0 @@
-package ict_common
-
-import (
-	"zzredis"
-)
-
-var GRedisClient zzredis.Client_t
--- package/src/mac_gen.sh
+++
@@ -1,2 +0,0 @@
-protoc --go_out=./pb_common pb_common.proto
-protoc --go_out=./pb_square pb_square.proto
